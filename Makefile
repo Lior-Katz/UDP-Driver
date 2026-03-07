901deecc4944dae9f00dd169f33c84af5eb0d97b
Lior Katz <liorkatz21@gmail.com>
2026-03-07 23:26:32 +0200

    add device registration

--- Makefile
+++ Makefile
@@ -21,6 +21,9 @@ endef
 module:
 	$(MAKE) -C $(KDIR) M=$$PWD
 
+install: $(MOD_NAME).ko
+	sudo insmod $(MOD_NAME).ko
+
 clean:
 	@if lsmod | grep -q "^$(MOD_NAME)"; then \
 		echo sudo rmmod $(MOD_NAME); \
@@ -29,14 +32,35 @@ clean:
 	rm -rf Module.symvers modules.order *.ko *.mod* *.o
 
 test: module
-	sudo insmod $(MOD_NAME).ko
-	$(call CHECK, dmesg | grep -q "$(MOD_NAME): module loaded", \
-		driver loaded, \
-		driver not loaded)
+	sudo dmesg -C
+	$(MAKE) install
 
 	$(call CHECK, grep -q "$(DRIVER_NAME)" /proc/devices, \
 		device major allocated, \
 		device major not allocated)
+
+	$(call CHECK, dmesg | grep -q "$(MOD_NAME): registered device", \
+		registered device, \
+		failed registering device)
+
+	$(call CHECK, dmesg | grep -q "$(MOD_NAME): module loaded", \
+		driver loaded, \
+		driver not loaded)
+
+# Create temporary device node
+	MAJOR=$$(awk '$2=="$(DRIVER_NAME)" {print $1}' /proc/devices) \
+	echo "Using major $$MAJOR"; \
+	sudo mknod /tmp/udp c $$MAJOR 0; \
+	sudo chmod 666 /tmp/udp
+
+# Write to device to trigger .write
+	@echo "test" > /tmp/udp
+
+# Check that write was called
+	$(call CHECK, dmesg | grep -q "$(MOD_NAME): write called", write invoked, write not invoked)
+
+# Cleanup device node
+	sudo rm -f /tmp/udp
 	
 	$(MAKE) clean
 
--- udp_write.c
+++ udp_write.c
@@ -2,6 +2,7 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/fs.h>
+#include <linux/cdev.h>
 
 #define MOD_NAME "udp_write"
 #define DEV_NAME ("udp")
@@ -10,28 +11,69 @@
 #define LOG(level, fmt, ...) \
     printk(KERN_##level MOD_NAME ": " fmt, ##__VA_ARGS__)
 
-static dev_t dev;
+ssize_t udp_write(struct file *filp, const char __user *buf, size_t count, loff_t *f_pos);
+static void __exit test_exit(void);
+
+static dev_t device_number;
+struct cdev* my_cdev = NULL;
+struct file_operations udp_fops = {
+	.owner =    THIS_MODULE,
+	.write =    udp_write,
+};
+
+ssize_t udp_write(struct file *filp, const char __user *buf, size_t count, loff_t *f_pos) {
+    LOG(INFO, "write called\n");
+    return count;
+}
 
 static int __init test_init(void)
 {
+    int result = 0;
     LOG(DEBUG, "allocating device numbers\n");
-    int result = alloc_chrdev_region(&dev, UDP_WRITE_MINOR, 1, DEV_NAME);
+    result = alloc_chrdev_region(&device_number, UDP_WRITE_MINOR, 1, DEV_NAME);
     if (result < 0) {
         LOG(WARNING, "can't allocate major\n");
         return result;
     }
-    int major = MAJOR(dev);
-    int minor = MINOR(dev);
+    int major = MAJOR(device_number);
+    int minor = MINOR(device_number);
     LOG(INFO, "allocated %d, %d\n", major, minor);
+
+    LOG(DEBUG, "registering device\n");
+    my_cdev = cdev_alloc();
+    if (my_cdev == NULL) {
+        LOG(WARNING, "Failed to allocate cdev\n");
+        result = -ENOMEM;
+        goto failure;
+    }
+    my_cdev->ops = &udp_fops;
+    my_cdev->owner = THIS_MODULE;
+    int err = cdev_add(my_cdev, device_number, 1);
+    if (err) {
+        LOG(NOTICE, "Error %d adding device\n", err);
+        return -1;
+    }
+    LOG(INFO, "registered device\n");
+
 
     LOG(INFO, "module loaded\n");
     return 0;
+
+failure:
+    test_exit();
+    return result;
 }
 
 static void __exit test_exit(void)
 {
+    LOG(DEBUG, "deleting device registration\n");
+    if (my_cdev != NULL) {
+        cdev_del(my_cdev);
+    }
+
     LOG(DEBUG, "unregistering device numbers\n");
-    unregister_chrdev_region(dev, 1);
+    unregister_chrdev_region(device_number, 1);
+
     LOG(INFO, "module unloaded\n");
 }
 
