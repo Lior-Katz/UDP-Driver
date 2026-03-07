53564ccbacd25d8526ee508c14a197c6a763e55a
Lior Katz <liorkatz21@gmail.com>
2026-03-07 21:38:50 +0200

    register major and minor number for device

--- Makefile
+++ Makefile
@@ -8,6 +8,16 @@ else
 # ----- Normal Makefile -----
 KDIR := /lib/modules/`uname -r`/build
 MOD_NAME := udp_write
+DRIVER_NAME := udp
+
+define CHECK
+@if $(1); then \
+	echo "SUCCESS: $(2)"; \
+else \
+	echo "FAIL: $(3)"; \
+fi
+endef
+
 module:
 	$(MAKE) -C $(KDIR) M=$$PWD
 
@@ -20,10 +30,22 @@ clean:
 
 test: module
 	sudo insmod $(MOD_NAME).ko
-	@if dmesg | grep -q "test_driver: module loaded"; then \
-    echo "SUCCESS: driver loaded"; \
-	else \
-    	echo "FAIL: driver not loaded"; \
-	fi
+	$(call CHECK, dmesg | grep -q "$(MOD_NAME): module loaded", \
+		driver loaded, \
+		driver not loaded)
+
+	$(call CHECK, grep -q "$(DRIVER_NAME)" /proc/devices, \
+		device major allocated, \
+		device major not allocated)
+	
+	$(MAKE) clean
+
+	$(call CHECK, ! lsmod | grep -q "^$(MOD_NAME)", \
+		module removed, \
+		module still loaded)
+
+	$(call CHECK, ! grep -q "$(DRIVER_NAME)" /proc/devices, \
+		device major unregistered, \
+		device major still registered)
 # ---------------------------
 endif
--- udp_write.c
+++ udp_write.c
@@ -1,16 +1,38 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
+#include <linux/fs.h>
+
+#define MOD_NAME "udp_write"
+#define DEV_NAME ("udp")
+#define UDP_WRITE_MINOR (0)
+ 
+#define LOG(level, fmt, ...) \
+    printk(KERN_##level MOD_NAME ": " fmt, ##__VA_ARGS__)
+
+static dev_t dev;
 
 static int __init test_init(void)
 {
-    printk(KERN_INFO "test_driver: module loaded\n");
+    LOG(DEBUG, "allocating device numbers\n");
+    int result = alloc_chrdev_region(&dev, UDP_WRITE_MINOR, 1, DEV_NAME);
+    if (result < 0) {
+        LOG(WARNING, "can't allocate major\n");
+        return result;
+    }
+    int major = MAJOR(dev);
+    int minor = MINOR(dev);
+    LOG(INFO, "allocated %d, %d\n", major, minor);
+
+    LOG(INFO, "module loaded\n");
     return 0;
 }
 
 static void __exit test_exit(void)
 {
-    printk(KERN_INFO "test_driver: module unloaded\n");
+    LOG(DEBUG, "unregistering device numbers\n");
+    unregister_chrdev_region(dev, 1);
+    LOG(INFO, "module unloaded\n");
 }
 
 module_init(test_init);
